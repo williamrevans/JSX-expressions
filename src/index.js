6fa8dbfa02b3112d39f6a9ea5ee91f0ac7d32796
tripleslicer <williamrevans@gmail.com>
2021-05-02 19:18:34 -0700

    create simple strings with expressions

--- src/index.js
+++ src/index.js
@@ -1,4 +1,17 @@
 import React from "react";
 import ReactDOM from "react-dom";
 
-ReactDOM.render(<h1>Hello World!</h1>, document.getElementById("root"));
+const name = "Will";
+const lastName = "Evans";
+const luckyNumber = 11;
+
+ReactDOM.render(
+  <div>
+    <h1>Hello {name + " " + lastName}!</h1>
+    <h2>
+      Hello {name} {lastName}!
+    </h2>
+    <p>Your lucky number is {luckyNumber}</p>
+  </div>,
+  document.getElementById("root")
+);
